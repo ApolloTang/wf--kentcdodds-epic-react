7b8491f05d72f97d006cc0b5dbc98246c53f34dc
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2022-12-25 07:25:54 -0500

    useeffect, lazy state init

---
+++ 2-react-hooks/02-01c-useEffect--lazy-state-init/app--before.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Greeting({initialName = ''}) {
+  const _initialName = window.localStorage.getItem('name') ?? initialName
+  const [name, setName] = React.useState(_initialName)
+
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name)
+  })
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting />
+}
+
+export {App}
