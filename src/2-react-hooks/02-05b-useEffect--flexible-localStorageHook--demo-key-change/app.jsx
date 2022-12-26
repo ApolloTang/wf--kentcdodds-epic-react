48f633d26d9b2412216f307a71eb7ade55d684ea
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2022-12-26 16:37:46 -0500

    added 02-05b-useEffect--flexible-localStorageHook--demo-key-change

---
+++ src/2-react-hooks/02-05b-useEffect--flexible-localStorageHook--demo-key-change/app.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+/**
+ *
+ * @param {String} key The key to set in localStorage for this value
+ * @param {Object} defaultValue The value to use if it is not already in localStorage
+ * @param {{serialize: Function, deserialize: Function}} options The serialize and deserialize functions to use (defaults to JSON.stringify and JSON.parse respectively)
+ */
+function useLocalStorageState(
+  key,
+  defaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      // the try/catch is here in case the localStorage value was set before
+      // we had the serialization in place (like we do in previous extra credits)
+      try {
+        return deserialize(valueInLocalStorage)
+      } catch (error) {
+        window.localStorage.removeItem(key)
+      }
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const prevKeyRef = React.useRef(key)
+
+  // Check the example at src/examples/local-state-key-change.js to visualize a key change
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, state, serialize])
+
+  return [state, setState]
+}
+
+
+function Greeting({initialName = ''}) {
+  const [key, setKey] = React.useState('name')
+  const [name, setName] = useLocalStorageState(key, initialName)
+
+  function handleClick() {
+    if (key === 'name') {
+      setKey('firstName')
+    } else if (key === 'firstName') {
+      setKey('Name')
+    } else {
+      setKey('name')
+    }
+  }
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  return (
+    <div>
+      <button type="button" onClick={handleClick}>
+        Change key!
+      </button>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting />
+}
+
+export {App}
