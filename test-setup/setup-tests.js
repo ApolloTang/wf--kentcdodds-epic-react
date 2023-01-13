51d835a632f57fb0d43fa0721924387225e7b820
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-01-12 21:58:17 -0500

    added whatwg-fetch

--- test-setup/setup-tests.js
+++ test-setup/setup-tests.js
@@ -1,5 +1,7 @@
 // This file will run on each test, after jest is loaded
 
+import 'whatwg-fetch' // Polyfill "window.fetch" used in the React component.
+
 import '@testing-library/jest-dom/extend-expect'
 
 
