7c9f346fdd640ae40d132ff1eb0d9b8a0850a8f2
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-05-06 16:00:01 -0400

    update main.tsx

--- react17/src/0-template/main.tsx
+++ react17/src/0-template/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import {render} from "react-dom";
 import {App} from "./app.jsx";
 
 const reactContainer = document.createElement('div')
@@ -7,8 +7,4 @@ reactContainer.innerText =  'react component goes here'
 reactContainer.id = 'root'
 document.body.appendChild(reactContainer)
 
-const root = createRoot(reactContainer!);
-root.render(<App />);
-// Ref: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
-
-
+render(<App />, reactContainer);
