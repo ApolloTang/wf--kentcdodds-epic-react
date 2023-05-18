16d7f034be85cf5660d70deeac6da3984255c38f
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-05-17 23:02:49 -0400

    update

---
+++ react18/src/3-advanced-react-hooks/03-02a-useContext-- refactor/app.jsx
@@ -0,0 +1,58 @@
+// useContext: simple Counter
+// http://localhost:3000/isolated/final/03.js
+
+import * as React from 'react'
+
+const CountContext = React.createContext()
+
+function CountProvider(props) {
+  const value = React.useState(0)
+  // Note: // value is actually:
+  //   const [count, setCount] = React.useState(0)
+  //   const value = [count, setCount]
+
+  return (
+    <CountContext.Provider
+      value={value}
+      {...props}
+    />
+  )
+}
+
+
+function useCount() {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error('useCount must be used within a CountProvider')
+  }
+  return context
+}
+
+
+function CountDisplay() {
+  const [count] = useCount()
+  return <div>{`The current count is ${count}`}</div>
+}
+
+
+function CounterControl() {
+  const [, setCount] = useCount()
+  const increment = () => setCount(c => c + 1)
+
+  return <button onClick={increment}>Increment count</button>
+}
+
+
+function App() {
+  return (
+    <div>
+      <CountProvider>
+        <CounterControl />
+        <CountDisplay />
+      </CountProvider>
+    </div>
+  )
+}
+
+
+export default App
