525a7d73846fa5b134b62561079b545ae3d767af
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-05-20 07:31:41 -0400

    remove unused code

--- react18/src/3-advanced-react-hooks/03-03b-useContext--extra2--1-start-by-me-from-02-03b/app.jsx
+++ react18/src/3-advanced-react-hooks/03-03b-useContext--extra2--1-start-by-me-from-02-03b/app.jsx
@@ -14,24 +14,6 @@ import {
 } from './pokemon'
 
 
-function asyncReducer(state, action) {
-  switch (action.type) {
-    case 'pending': {
-      return {status: 'pending', data: null, error: null}
-    }
-    case 'resolved': {
-      return {status: 'resolved', data: action.data, error: null}
-    }
-    case 'rejected': {
-      return {status: 'rejected', data: null, error: action.error}
-    }
-    default: {
-      throw new Error(`Unhandled action type: ${action.type}`)
-    }
-  }
-}
-
-
 function PokemonInfo({pokemonName}) {
   const { data: pokemon, status, error, run } = useAsync({
     status: pokemonName ? 'pending' : 'idle',
