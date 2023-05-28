68c1abfa62a77130edcec75c8cefd3f64cc44deb
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-05-28 04:24:41 -0400

    added: style

--- react18/src/3-advanced-react-hooks/04-01b-useLayoutEffect--2-useLayoutEffect/main.tsx
+++ react18/src/3-advanced-react-hooks/04-01b-useLayoutEffect--2-useLayoutEffect/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import App from "./app.jsx";
+import './styles.css'
 
 const reactContainer = document.createElement('div')
 reactContainer.innerText =  'react component goes here'
